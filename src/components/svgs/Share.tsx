a3ef18a5c2165dc533a70a94aad3b38f571672f4
carokann1945 <carokann1945@gmail.com>
2026-03-04 13:41:25 +0900

    fix : build error

--- src/components/svgs/Share.tsx
+++ src/components/svgs/Share.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIconShare = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={15} height={13} {...props}>
